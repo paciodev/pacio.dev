5a4f10dabe0aa360c63238dc6693d8d3dc2230ec
PacioTenPros <paciobumbum@gmail.com>
2023-04-22 23:13:06 +0200

    ProjectCard animation change

--- components/About.tsx
+++ components/About.tsx
@@ -12,158 +12,158 @@ import '@/assets/css/glitch.css';
 import { Element } from 'react-scroll';
 
 const card = {
-  hover: {
-    scale: 1.05,
-    transition: { type: 'spring', duration: 0.5, bounce: 0.6 },
-  },
+	hover: {
+		scale: 1.05,
+		transition: { type: 'spring', duration: 0.5, bounce: 0.6 },
+	},
 };
 
 const About = () => {
-  const [text] = useTypewriter({
-    words: ['Web Developer', 'Always inspired', 'Young mind', 'Fresh POV'],
-    loop: 0,
-    delaySpeed: 2000,
-  });
+	const [text] = useTypewriter({
+		words: ['Web Developer', 'Always inspired', 'Young mind', 'Fresh POV'],
+		loop: 0,
+		delaySpeed: 2000,
+	});
 
-  return (
-    <motion.div
-      initial={{ scale: 0 }}
-      animate={{
-        scale: 1,
-        transition: { delay: 1, duration: 0.5, type: 'spring' },
-      }}
-      className='max-w-6xl mx-auto font-extrabold px-0 sm:px-10 md:px-32 xl:px-0 mt-[30vh]'
-      id='about'
-    >
-      <Element className='bg-red-light' name='about'>
-        <h2 className='text-center py-6 text-3xl text-red-dark'>
-          Facts about me
-        </h2>
-        <div className='grid grid-cols-2 mini:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 p-5 lg:px-24 pb-16 !pt-0 text-white gap-3'>
-          <motion.div
-            variants={card}
-            whileHover='hover'
-            className='h-32 rounded-xl col-span-2 bg-red-dark flex flex-col items-center justify-center bg-[url(/assets/img/pl.webp)] bg-cover bg-center space-y-6'
-          >
-            <p className='text-3xl text-[#DD0C39]'>I am from</p>
-            <p className='text-3xl'>Poland</p>
-          </motion.div>
+	return (
+		<motion.div
+			initial={{ scale: 0 }}
+			animate={{
+				scale: 1,
+				transition: { delay: 1, duration: 0.5, type: 'spring' },
+			}}
+			className='max-w-6xl mx-auto font-extrabold px-0 sm:px-10 md:px-32 xl:px-0 mt-[30vh]'
+			id='about'
+		>
+			<Element className='bg-red-light' name='about'>
+				<h2 className='text-center py-6 text-3xl text-red-dark'>
+					Facts about me
+				</h2>
+				<div className='grid grid-cols-2 mini:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 p-5 lg:px-24 pb-16 !pt-0 text-white gap-3'>
+					<motion.div
+						variants={card}
+						whileHover='hover'
+						className='h-32 rounded-xl col-span-2 bg-red-dark flex flex-col items-center justify-center bg-[url(/assets/img/pl.webp)] bg-cover bg-center space-y-6'
+					>
+						<p className='text-3xl text-[#DD0C39]'>I am from</p>
+						<p className='text-3xl'>Poland</p>
+					</motion.div>
 
-          <motion.div
-            variants={card}
-            whileHover='hover'
-            className='h-32 rounded-xl bg-red-dark flex flex-col items-center justify-center'
-          >
-            <p className='-mt-[22px]'>Age:</p>
-            <p className='text-5xl leading-[0.8]'>
-              {getPassedYearsFromDate('2007/12/09')}
-            </p>
-          </motion.div>
+					<motion.div
+						variants={card}
+						whileHover='hover'
+						className='h-32 rounded-xl bg-red-dark flex flex-col items-center justify-center'
+					>
+						<p className='-mt-[22px]'>Age:</p>
+						<p className='text-5xl leading-[0.8]'>
+							{getPassedYearsFromDate('2007/12/09')}
+						</p>
+					</motion.div>
 
-          <motion.div
-            variants={card}
-            whileHover='hover'
-            className='h-32 rounded-xl bg-red-dark flex flex-col items-center justify-center'
-          >
-            <p>Over</p>
-            <p className='text-5xl leading-[0.8]'>30</p>
-            <p>projects</p>
-          </motion.div>
+					<motion.div
+						variants={card}
+						whileHover='hover'
+						className='h-32 rounded-xl bg-red-dark flex flex-col items-center justify-center'
+					>
+						<p>Over</p>
+						<p className='text-5xl leading-[0.8]'>30</p>
+						<p>projects</p>
+					</motion.div>
 
-          <motion.div
-            variants={card}
-            whileHover='hover'
-            className='h-32 rounded-xl col-span-2 bg-red-dark flex flex-col items-center justify-center text-2xl'
-          >
-            <p>Coding for:</p>
-            <p>
-              {getPassedYearsFromDate('2021/12/21')}+ year
-              {getPassedYearsFromDate('2021/12/21') !== 1 && 's'}
-            </p>
-          </motion.div>
+					<motion.div
+						variants={card}
+						whileHover='hover'
+						className='h-32 rounded-xl col-span-2 bg-red-dark flex flex-col items-center justify-center text-2xl'
+					>
+						<p>Coding for:</p>
+						<p>
+							{getPassedYearsFromDate('2021/12/21')}+ year
+							{getPassedYearsFromDate('2021/12/21') !== 1 && 's'}
+						</p>
+					</motion.div>
 
-          <motion.div
-            variants={card}
-            whileHover='hover'
-            className='h-32 rounded-xl col-span-2 bg-red-dark flex flex-col items-center justify-center text-2xl space-y-2'
-          >
-            <p>Dogs &gt; cats</p>
-            <Image src={doggo} alt='' width={80} />
-          </motion.div>
+					<motion.div
+						variants={card}
+						whileHover='hover'
+						className='h-32 rounded-xl col-span-2 bg-red-dark flex flex-col items-center justify-center text-2xl space-y-2'
+					>
+						<p>Dogs &gt; cats</p>
+						<Image src={doggo} alt='' width={80} />
+					</motion.div>
 
-          <motion.div
-            variants={card}
-            whileHover='hover'
-            className='h-32 rounded-xl col-span-2 bg-red-dark flex flex-col items-center justify-center text-2xl bg-img bg-pizza'
-          >
-            <p>In love with</p>
-          </motion.div>
+					<motion.div
+						variants={card}
+						whileHover='hover'
+						className='h-32 rounded-xl col-span-2 bg-red-dark flex flex-col items-center justify-center text-2xl bg-img bg-pizza'
+					>
+						<p>In love with</p>
+					</motion.div>
 
-          <motion.div
-            variants={card}
-            whileHover='hover'
-            className='min-h-[8rem] rounded-xl col-span-2 mini:col-span-4 row-start-4 mini:row-start-3 lg:row-start-auto row-span-1 xl:row-span-2 bg-red-dark relative'
-          >
-            <Image
-              src={pacio}
-              alt='My photo that was taken in London'
-              fill
-              className='rounded-xl object-cover'
-            />
-          </motion.div>
+					<motion.div
+						variants={card}
+						whileHover='hover'
+						className='min-h-[8rem] rounded-xl col-span-2 mini:col-span-4 row-start-4 mini:row-start-3 lg:row-start-auto row-span-1 xl:row-span-2 bg-red-dark relative'
+					>
+						<Image
+							src={pacio}
+							alt='My photo that was taken in London'
+							fill
+							className='rounded-xl object-cover'
+						/>
+					</motion.div>
 
-          <motion.div
-            variants={card}
-            whileHover='hover'
-            className='h-[268px] mini:h-auto rounded-xl col-start-auto lg:col-start-5 xl:col-start-auto col-span-2 row-span-2 lg:row-span-auto lg:row-start-2 lg:row-end-4 xl:row-span-2 bg-red-dark bg-hobbies'
-          />
+					<motion.div
+						variants={card}
+						whileHover='hover'
+						className='h-[268px] mini:h-auto rounded-xl col-start-auto lg:col-start-5 xl:col-start-auto col-span-2 row-span-2 lg:row-span-auto lg:row-start-2 lg:row-end-4 xl:row-span-2 bg-red-dark bg-hobbies'
+					/>
 
-          <motion.div
-            variants={card}
-            whileHover='hover'
-            className='h-32 rounded-xl col-span-2 bg-red-dark flex lg:hidden xl:flex flex-col items-center justify-center text-4xl bg-img bg-vscode'
-          >
-            <p>Using</p>
-          </motion.div>
+					<motion.div
+						variants={card}
+						whileHover='hover'
+						className='h-32 rounded-xl col-span-2 bg-red-dark flex lg:hidden xl:flex flex-col items-center justify-center text-4xl bg-img bg-vscode'
+					>
+						<p>Using</p>
+					</motion.div>
 
-          <motion.div
-            variants={card}
-            whileHover='hover'
-            className='text-center h-32 rounded-xl col-span-2 mini:col-span-4 bg-red-dark flex items-center justify-center text-3xl mini:text-4xl'
-          >
-            <p>{text}</p>
-            <Cursor />
-          </motion.div>
+					<motion.div
+						variants={card}
+						whileHover='hover'
+						className='text-center h-32 rounded-xl col-span-2 mini:col-span-4 bg-red-dark flex items-center justify-center text-3xl mini:text-4xl'
+					>
+						<p>{text}</p>
+						<Cursor />
+					</motion.div>
 
-          <motion.div
-            variants={card}
-            whileHover='hover'
-            className='h-32 rounded-xl col-span-1 bg-zinc-800 grid place-content-center'
-          >
-            <Image src={react} alt='I work with React' width={70} />
-          </motion.div>
+					<motion.div
+						variants={card}
+						whileHover='hover'
+						className='h-32 rounded-xl col-span-1 bg-zinc-800 grid place-content-center'
+					>
+						<Image src={react} alt='I work with React' width={70} />
+					</motion.div>
 
-          <motion.div
-            variants={card}
-            whileHover='hover'
-            className='h-32 rounded-xl col-span-1 bg-zinc-800 grid place-content-center'
-          >
-            <Image src={tailwind} alt='I work with Tailwind CSS' width={70} />
-          </motion.div>
+					<motion.div
+						variants={card}
+						whileHover='hover'
+						className='h-32 rounded-xl col-span-1 bg-zinc-800 grid place-content-center'
+					>
+						<Image src={tailwind} alt='I work with Tailwind CSS' width={70} />
+					</motion.div>
 
-          <motion.div
-            variants={card}
-            whileHover='hover'
-            className='h-32 rounded-xl col-span-2 bg-black flex lg:hidden xl:flex flex-col items-center justify-center text-center text-5xl cursor-wait select-none'
-          >
-            <p className='glitch' data-text='???'>
-              ???
-            </p>
-          </motion.div>
-        </div>
-      </Element>
-    </motion.div>
-  );
+					<motion.div
+						variants={card}
+						whileHover='hover'
+						className='h-32 rounded-xl col-span-2 bg-black flex lg:hidden xl:flex flex-col items-center justify-center text-center text-5xl cursor-wait select-none'
+					>
+						<p className='glitch' data-text='???'>
+							???
+						</p>
+					</motion.div>
+				</div>
+			</Element>
+		</motion.div>
+	);
 };
 
 export default About;
--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -6,69 +6,64 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 const ProjectCard = ({ project }: { project: Project }) => {
-  return (
-    <div>
-      <Link href={`/projects/${project.slug.current}`} className='outline-none'>
-        <motion.div
-          initial={{
-            scale: 0.7,
-            backgroundSize: `${project.previewImageSize || 70}%`,
-          }}
-          whileHover={{
-            backgroundSize: `${(project.previewImageSize || 70) - 3}%`,
-            transition: {
-              duration: 0.1,
-              ease: 'easeOut',
-            },
-          }}
-          whileInView={{
-            scale: 1,
-            transition: { type: 'spring', duration: 0.5 },
-          }}
-          viewport={{ once: true }}
-          className='mini:py-6 xl:py-16 hover:xl:py-20 hover:xl:px-24 transition-all ease-out mini:px-12 xl:px-20 flex flex-col mini:justify-between mini:h-[400px] xl:h-[600px] relative bg-no-repeat mini-bg-none'
-          style={{
-            backgroundColor: project.colorLight.hex,
-            backgroundImage: `url('${urlFor(project.previewImage).url()}')`,
-            backgroundPosition: `right ${
-              project.previewImagePosition || 'center'
-            }`,
-            color: project.colorDark.hex,
-          }}
-        >
-          <h2 className='px-8 mini:px-0 font-extrabold text-4xl xl:text-[58px] pt-6 mini:pt-0'>
-            {project.name}
-          </h2>
-          <div>
-            <p className='px-8 mini:px-0 font-extrabold text-2xl xl:text-4xl'>
-              {project.company}
-            </p>
-            <div className='mini:flex px-8 mini:px-0 mini:space-x-3 space-y-1 mini:space-y-0 mt-1 mb-3'>
-              {project.categories.map((c) => (
-                <div
-                  style={{
-                    backgroundColor: project.colorDark.hex,
-                    color: project.colorLight.hex,
-                  }}
-                  key={c._id}
-                  className='px-5 py-[2px] w-full mini:w-auto rounded-full text-center overflow-hidden'
-                >
-                  {c.title}
-                </div>
-              ))}
-            </div>
-            <Image
-              src={urlFor(project.image).url()}
-              alt={`Image of ${project.name}`}
-              className='mini:hidden mb-3 mx-auto'
-              width={300}
-              height={200}
-            />
-          </div>
-        </motion.div>
-      </Link>
-    </div>
-  );
+	return (
+		<div>
+			<Link href={`/projects/${project.slug.current}`} className='outline-none'>
+				<motion.div
+					initial={{
+						backgroundSize: `${project.previewImageSize || 70}%`,
+					}}
+					whileHover={{
+						backgroundSize: `${(project.previewImageSize || 70) - 1}%`,
+						transition: {
+							duration: 0.1,
+							ease: 'easeOut',
+						},
+					}}
+					viewport={{ once: true }}
+					className='mini:py-6 xl:py-16 transition-all ease-out mini:px-12 xl:px-20 flex flex-col mini:justify-between mini:h-[400px] xl:h-[600px] relative bg-no-repeat mini-bg-none'
+					style={{
+						backgroundColor: project.colorLight.hex,
+						backgroundImage: `url('${urlFor(project.previewImage).url()}')`,
+						backgroundPosition: `right ${
+							project.previewImagePosition || 'center'
+						}`,
+						color: project.colorDark.hex,
+					}}
+				>
+					<h2 className='px-8 mini:px-0 font-extrabold text-4xl xl:text-[58px] pt-6 mini:pt-0'>
+						{project.name}
+					</h2>
+					<div>
+						<p className='px-8 mini:px-0 font-extrabold text-2xl xl:text-4xl'>
+							{project.company}
+						</p>
+						<div className='mini:flex px-8 mini:px-0 mini:space-x-3 space-y-1 mini:space-y-0 mt-1 mb-3'>
+							{project.categories.map((c) => (
+								<div
+									style={{
+										backgroundColor: project.colorDark.hex,
+										color: project.colorLight.hex,
+									}}
+									key={c._id}
+									className='px-5 py-[2px] w-full mini:w-auto rounded-full text-center overflow-hidden'
+								>
+									{c.title}
+								</div>
+							))}
+						</div>
+						<Image
+							src={urlFor(project.image).url()}
+							alt={`Image of ${project.name}`}
+							className='mini:hidden mb-3 mx-auto'
+							width={300}
+							height={200}
+						/>
+					</div>
+				</motion.div>
+			</Link>
+		</div>
+	);
 };
 
 export default ProjectCard;
